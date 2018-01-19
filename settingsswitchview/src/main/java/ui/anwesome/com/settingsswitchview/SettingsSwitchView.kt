c8bf134349d66f83e915eb5a52923ee5b67c97eb
Anwesh43 <anweshthecool0@gmail.com>
2018-01-19 07:58:53 +0530

    adding Animator

--- settingsswitchview/src/main/java/ui/anwesome/com/settingsswitchview/SettingsSwitchView.kt
+++ settingsswitchview/src/main/java/ui/anwesome/com/settingsswitchview/SettingsSwitchView.kt
@@ -82,4 +82,29 @@ class SettingsSwitchView(ctx:Context):View(ctx) {
             }
         }
     }
+    data class Animator(var view:SettingsSwitchView,var animated:Boolean = false) {
+        fun animate(updatecb:()->Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex:Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
