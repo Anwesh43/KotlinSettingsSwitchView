359d4ca658d23539312e5ee60234eb94a8343d30
Anwesh43 <anweshthecool0@gmail.com>
2018-01-19 07:42:02 +0530

    adding settingsswitchstate

--- settingsswitchview/src/main/java/ui/anwesome/com/settingsswitchview/SettingsSwitchView.kt
+++ settingsswitchview/src/main/java/ui/anwesome/com/settingsswitchview/SettingsSwitchView.kt
@@ -45,4 +45,21 @@ class SettingsSwitchView(ctx:Context):View(ctx) {
 
         }
     }
+    data class SettingsSwitchState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1 - 2*scale
+                startcb()
+            }
+        }
+    }
 }
