ff748b1372d0f080d4873641cfa03fcd439cdaae
Anwesh43 <anweshthecool0@gmail.com>
2018-01-19 07:39:34 +0530

    adding settingsswitch class

--- settingsswitchview/src/main/java/ui/anwesome/com/settingsswitchview/SettingsSwitchView.kt
+++ settingsswitchview/src/main/java/ui/anwesome/com/settingsswitchview/SettingsSwitchView.kt
@@ -14,9 +14,35 @@ class SettingsSwitchView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class SettingsSwitch(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.save()
+            canvas.rotate(90f)
+            canvas.restore()
+            canvas.save()
+            canvas.translate(-size/6,0f)
+            canvas.save()
+            paint.color = Color.parseColor("#EEEEEE")
+            canvas.drawRoundRect(RectF(0f,-size/10,size/3,size/10),size/10,size/10,paint)
+            canvas.drawCircle(-size/10+size/7+size/3,0f,size/7,paint)
+            paint.color = Color.parseColor("#01579B")
+            canvas.drawRoundRect(RectF(0f,-size/10,size/3,size/10),size/10,size/10,paint)
+            canvas.restore()
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
     }
 }
